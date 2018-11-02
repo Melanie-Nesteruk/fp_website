cd0ef370f2aa653edab5a6dcd2d398919a870613
andrewmis <amis@kent.edu>
2018-11-02 09:57:33 -0400

    Fixed error in routes.py

--- app/routes.py
+++ app/routes.py
@@ -11,47 +11,47 @@ userInstance = User()
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", title='Home', app=app, loggedIn=userInstance.isLoggedIn(), userInstance=userInstance)
+    return render_template("index.html", title='Home', app=app, loggedIn=userInstance.getLoginState(), userInstance=userInstance)
 
 @app.route('/login')
 def login():
-    return render_template("login.html", title='Login', app=app, loggedIn=userInstance.isLoggedIn(), userInstance=userInstance)
+    return render_template("login.html", title='Login', app=app, loggedIn=userInstance.getLoginState(), userInstance=userInstance)
 
 @app.route('/register')
 def register():
-    return render_template("register.html", title='Register', app=app, loggedIn=userInstance.isLoggedIn(), userInstance=userInstance)
+    return render_template("register.html", title='Register', app=app, loggedIn=userInstance.getLoginState(), userInstance=userInstance)
 
 @app.route('/about')
 def about():
-    return render_template("about.html", title='About', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("about.html", title='About', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/work')
 def products():
-    return render_template("work.html", title='Work', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("work.html", title='Work', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/events')
 def store():
-    return render_template("events.html", title='Events', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("events.html", title='Events', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/socialmedia')
 def socialMedia():
-    return render_template("socialmedia.html", title='Social Media', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("socialmedia.html", title='Social Media', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/subscribe')
 def subscribe():
-    return render_template("subscribe.html", title='Subscribe', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("subscribe.html", title='Subscribe', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/directory')
 def directory():
-    return render_template("directory.html", title='Directory', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("directory.html", title='Directory', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/profile')
 def profile():
-    return render_template("profile.html", title='Profile', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("profile.html", title='Profile', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/messages')
 def messages():
-    return render_template("messages.html", title='Messages', app=app, loggedIn=userInstance.isLoggedIn())
+    return render_template("messages.html", title='Messages', app=app, loggedIn=userInstance.getLoginState())
 
 @app.route('/jsLogin', methods = ['POST'])
 def jsLogin():
