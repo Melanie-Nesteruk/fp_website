7a1ba27deac6ea8a6fd90bcec1ac134d36445d16
andrewmis <amis@kent.edu>
2018-11-05 14:26:03 -0500

    Fixing navigation

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,4 +1,78 @@
 (function() {
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+        var config = {
+        apiKey: "AIzaSyCEuT1gco387t16C2IAmN2bx5bt-n6ea6s",
+        authDomain: "focal-point-student-alumni-net.firebaseapp.com",
+        databaseURL: "https://focal-point-student-alumni-net.firebaseio.com",
+        projectId: "focal-point-student-alumni-net",
+        storageBucket: "focal-point-student-alumni-net.appspot.com",
+        messagingSenderId: "1002904582612"
+        };
+        firebase.initializeApp(config);
+    }
+
+    if (firebase.auth().currentUser)
+    {
+        // Show "Profile", "Directory", "Messages", and "Logout"
+        setNavigation(true);
+    }
+    else
+    {
+        // Show "Mailing List" and "Login"
+        setNavigation(false);
+    }
+        
+    function setNavigation(loggedIn)
+    {
+        var tempFragment;
+        if (loggedIn)
+        {
+            document.getElementById("subscibeNav").remove();
+            document.getElementById("LoginNav").remove();
+        }
+        else
+        {
+            // Add "Mailing List" nav link
+            var node = document.createElement("LI");
+            node.classList.add("nav-item");
+            node.classList.add("px-lg-4");
+            node.id = "subscribeNav";
+            
+            var linkNode = document.createElement("a");
+            linkNode.classList.add("nav-link");
+            linkNode.classList.add("text-uppercase");
+            linkNode.classList.add("text-expanded");
+
+            linkNode.id = "subscribe";
+            linkNode.href = "/subscribe";
+            var textNode = document.createTextNode("Mailing List");
+
+            linkNode.appendChild(textNode); 
+            node.appendChild(linkNode);
+            document.getElementById("navBarList").appendChild(node);
+
+            // --------------------------------------------------
+
+            // Add "Login" nav link
+            var node = document.createElement("LI");
+            node.classList.add("nav-item");
+            node.classList.add("px-lg-4");
+            node.id = "loginNav";
+            
+            var linkNode = document.createElement("a");
+            linkNode.classList.add("nav-link");
+            linkNode.classList.add("text-uppercase");
+            linkNode.classList.add("text-expanded");
+            linkNode.id = "login";
+            linkNode.href = "/login";
+            var textNode = document.createTextNode("Login");
+
+            linkNode.appendChild(textNode); 
+            node.appendChild(linkNode);
+            document.getElementById("navBarList").appendChild(node);
+        }
+    }
 
     var currentPage = document.currentScript.getAttribute('title').toLowerCase();
     var navLinks = {
@@ -26,7 +100,5 @@
             }
         }
     };
-
-    
 
 }());
