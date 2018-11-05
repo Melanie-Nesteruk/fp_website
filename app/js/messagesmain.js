2946005bff77c806b89b06e08758139e0b101ee3
Eugene Morrison <emorri28@kent.edu>
2018-11-04 23:38:54 -0500

    message changes

--- app/js/messagesmain.js
+++ app/js/messagesmain.js
@@ -49,7 +49,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 	email = user.email;
 	photoUrl = user.photoURL;
 	emailVerified = user.emailVerified;
-	uid = user.getToken();  // The user's ID, unique to the Firebase project. Do NOT use
+	uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
 			         // this value to authenticate with your backend server, if
 			         // you have one. Use User.getToken() instead.
 					 console.log('name: ', name);
