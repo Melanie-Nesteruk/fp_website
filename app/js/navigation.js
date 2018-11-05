8d98833f6067ee66e186639a014636255da64303
andrewmis <amis@kent.edu>
2018-11-05 16:21:35 -0500

    Updating logout link in navigation.js

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -117,8 +117,8 @@
                 linkNode.classList.add("text-uppercase");
                 linkNode.classList.add("text-expanded");
                 linkNode.id = "logout";
-                linkNode.href = "/logout.html";
-                linkNode.onclick="return false;";
+                linkNode.href = "logout.html";
+                linkNode.onclick.add("return&nbsp;false;");
                 var textNode = document.createTextNode("Logout");
     
                 linkNode.appendChild(textNode); 
