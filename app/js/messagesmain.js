10cb60db9a05943508883dfdad60440532e4d6c7
Eugene Morrison <emorri28@kent.edu>
2018-11-06 18:21:20 -0500

    message changes

--- app/js/messagesmain.js
+++ app/js/messagesmain.js
@@ -42,7 +42,7 @@ function renderFriendsList(doc){
 	button.setAttribute('value', doc.id);
 	// 'element'.textContent = doc.data().'element';
 	
-	button.addEventListener('click', openMessengerWith());
+	button.addEventListener('click', openMessengerWith);
 	
 	connectedFriendsList.appendChild(button);
 	console.log('Friend listed.');
