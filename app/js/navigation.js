c8620c510a73b9676cfb8cf42d4b0fd325ebcf13
andrewmis <amis@kent.edu>
2018-11-09 17:14:06 -0500

    Fixed navigation.js error in setNavigation()

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -37,7 +37,7 @@
     function setNavigation(loggedIn) {
         var tempFragment;
         if (loggedIn) {
-            user = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+            var user = FirebaseAuth.getInstance().getCurrentUser().getEmail();
             var subscribe = document.getElementById("subscribeNav");
             var login = document.getElementById("loginNav");
             if (subscribe) subscribe.remove();
